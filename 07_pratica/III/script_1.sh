3c44f239f786ddc66deb1a7d195dbf8280d618c5
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2020-09-23 17:55:34 -0300

    realização parte III

---
+++ 07_pratica/III/script_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Script a)"
+exemplo() {
+echo $FUNCNAME says hello
+}
+echo "chamar a função..."
+exemplo
+echo "repete..."
+exemplo
+echo
---
+++ 07_pratica/III/script_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Script b)"
+say () {
+echo "I say, " $1
+}
+say hello
+say hello hello
+say "hello hello hello"
+echo
