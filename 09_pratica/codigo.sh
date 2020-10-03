e688f419176552342ae4ece0eabcc568f7e516fe
Anarco Nascimento <anarco.nascimento@ccc.ufcg.edu.br>
2020-10-03 02:09:29 -0300

    Bloquando um usuario

--- 09_pratica/codigo.sh
+++ 09_pratica/codigo.sh
@@ -3,7 +3,15 @@
 SUCCESS=0        
 ERROR=1
 
-LAST=$(last -s -$2days -t today) #retorna todos os logins desde $2 dias atrás até hoje
+LAST_LOGGINS=$(last -s -$2days -t today | head -n -2) #retorna todos os logins desde $2 dias atrás até hoje
+
+if [ -n "$LAST_LOGGINS" ]
+then
+	echo "OK. Users found"
+else 
+	echo "No users logged in in the determined period"
+	exit $ERROR
+fi
 
 L=$(grep "^UID_MIN" /etc/login.defs)
 L1=$(grep "^UID_MAX" /etc/login.defs)
@@ -12,9 +20,27 @@ USERS=$(awk -F':' -v "min=${L##UID_MIN}" -v "max=${L1##UID_MAX}" '{ if ( $3 >= m
 
 case "$1" in
 	-block )
-	
+		for user in $USERS
+		do
+			if [[ $LAST_LOGGINS =~ .^${user}.* ]]
+			then 
+				echo "NOT BLOCKING USER ${user}"
+			else
+				echo "BLOCKING USER ${user}" # Os usuarios que não estiverem na lista do LAST_LOGGINS serão bloqueados
+				sudo passwd -lock ${user}
+			fi
+		done
 	;;
 	-remove )
+		for user in $USERS
+		do
+			if [[ $LAST_LOGGINS =~ .^${user}.* ]]
+			then 
+				echo "NOT REMOVING USER ${user}"
+			else
+				echo "BACKING UP FILES FROM /home/${user}" # Os usuarios que não estiverem na lista do LAST_LOGGINS serão removidos do sistema
+			fi
+		done
 	
 	;;
 	*)
@@ -22,3 +48,5 @@ case "$1" in
 		exit $ERROR
 	;;
 esac
+
+exit $SUCCESS
